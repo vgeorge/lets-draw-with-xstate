f8b8d0f697ed0d570c89f2fe0c659d2e81ae6c9b
Vitor George <vitor.george@gmail.com>
2025-02-14 16:54:33 +0000

    Empty map

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Map from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+const SidePanel = () => {
+  return (
+    <div
+      style={{
+        width: "300px",
+        height: "100%",
+        backgroundColor: "#f0f0f0",
+        padding: "20px",
+        boxShadow: "2px 0 5px rgba(0,0,0,0.1)",
+        boxSizing: "border-box",
+      }}
+    >
+      <h1 style={{ fontSize: "1.5em", marginBottom: "10px" }}>Side Panel</h1>
+      <p style={{ fontSize: "1em", color: "#333" }}>Let's draw an rectangle!</p>
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ height: "100vh", display: "flex" }}>
+      <SidePanel />
+      <Map
+        initialViewState={{
+          longitude: -100,
+          latitude: 40,
+          zoom: 3.5,
+        }}
+        style={{ width: "100%" }}
+        mapStyle="https://demotiles.maplibre.org/style.json"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
